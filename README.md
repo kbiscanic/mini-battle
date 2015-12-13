b36e008d45b8679d02fed09773d628f8bab469ae
Kristijan Biscanic <kbiscanic@gmail.com>
2015-12-13 13:05:58 +0100

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # mini-battle
+### Description
+This project simulates mini battle of 2 armies, each consisting of N soldiers.
+Soldiers are generated and battle is simulated using**very**simplified version
+of [Star Wars: Age of Rebellion RPG (FFG)](https://www.fantasyflightgames.com/en/products/star-wars-age-of-rebellion/)
+core rules.
+
+Implementation is entirely written in Java 8 and has no user interface - only
+textual output of results.
+
+### Usage
+```bash
+git clone https://github.com/kbiscanic/mini-battle.git
+
+mvn package
+
+# N = number of soldiers in each army
+java -jar target/mini-battle-1.0-SNAPSHOT-jar-with-dependencies.jar N
+```
--- src/main/java/org/kbiscanic/minibattle/Main.java
+++ src/main/java/org/kbiscanic/minibattle/Main.java
@@ -6,12 +6,25 @@ import org.kbiscanic.minibattle.armies.Team;
 public class Main {
 
     public static void main(String[] args) {
+        int n = 0;
         if (args.length != 1) {
             System.err.println("Wrong number of arguments. Expected: 1");
             System.exit(-1);
+        } else {
+            try {
+                n = Integer.parseInt(args[0]);
+            } catch (NumberFormatException nex) {
+                System.err.println("Argument must be a number.");
+                System.exit(-1);
+            }
         }
 
-        Battle battle = new Battle(new Army(Team.RED, Integer.parseInt(args[0]), 5), new Army(Team.BLUE, Integer.parseInt(args[0]), 5));
+        if (n < 1) {
+            System.err.println("N must be > 0");
+            System.exit(-1);
+        }
+
+        Battle battle = new Battle(new Army(Team.RED, n, 5), new Army(Team.BLUE, n, 5));
 
         battle.run();
     }
